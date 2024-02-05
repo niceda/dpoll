a508e4abf8a187e2794447c65e39a156fc0724f0
XiongDajun <iwanderuu@gmail.com>
2024-02-05 18:11:44 +0800

    chore: Update dependencies and clean up file imports
    - Update tokio-modbus to include additional features from the main branch
    - Update the version of `futures` to `0.3.30`
    - Update the year in the SPDX-FileCopyrightText to `2024` in `tcp-server.rs`
    - Remove the `time` import in `tcp-server.rs`
    - Delete the file `rtu-server-address.rs`

--- examples/tcp-server.rs
+++ examples/tcp-server.rs
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: Copyright (c) 2017-2023 slowtec GmbH <post@slowtec.de>
+// SPDX-FileCopyrightText: Copyright (c) 2017-2024 slowtec GmbH <post@slowtec.de>
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
 //! # TCP server example
@@ -8,12 +8,11 @@
 
 use std::{
     collections::HashMap,
+    future,
     net::SocketAddr,
     sync::{Arc, Mutex},
-    time::Duration,
 };
 
-use futures::future;
 use tokio::net::TcpListener;
 
 use tokio_modbus::{
@@ -28,9 +27,7 @@ struct ExampleService {
 
 impl tokio_modbus::server::Service for ExampleService {
     type Request = Request<'static>;
-    type Response = Response;
-    type Error = std::io::Error;
-    type Future = future::Ready<Result<Self::Response, Self::Error>>;
+    type Future = future::Ready<Result<Response, Exception>>;
 
     fn call(&self, req: Self::Request) -> Self::Future {
         match req {
@@ -67,11 +64,7 @@ impl tokio_modbus::server::Service for ExampleService {
             }
             _ => {
                 println!("SERVER: Exception::IllegalFunction - Unimplemented function code in request: {req:?}");
-                // TODO: We want to return a Modbus Exception response `IllegalFunction`. https://github.com/slowtec/tokio-modbus/issues/165
-                future::ready(Err(std::io::Error::new(
-                    std::io::ErrorKind::AddrNotAvailable,
-                    "Unimplemented function code in request".to_string(),
-                )))
+                future::ready(Err(Exception::IllegalFunction))
             }
         }
     }
@@ -88,7 +81,6 @@ impl ExampleService {
         holding_registers.insert(1, 20);
         holding_registers.insert(2, 30);
         holding_registers.insert(3, 40);
-        holding_registers.insert(16, 40);
         Self {
             input_registers: Arc::new(Mutex::new(input_registers)),
             holding_registers: Arc::new(Mutex::new(holding_registers)),
@@ -101,7 +93,7 @@ fn register_read(
     registers: &HashMap<u16, u16>,
     addr: u16,
     cnt: u16,
-) -> Result<Vec<u16>, std::io::Error> {
+) -> Result<Vec<u16>, Exception> {
     let mut response_values = vec![0; cnt.into()];
     for i in 0..cnt {
         let reg_addr = addr + i;
@@ -110,10 +102,7 @@ fn register_read(
         } else {
             // TODO: Return a Modbus Exception response `IllegalDataAddress` https://github.com/slowtec/tokio-modbus/issues/165
             println!("SERVER: Exception::IllegalDataAddress");
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::AddrNotAvailable,
-                format!("no register at address {reg_addr}"),
-            ));
+            return Err(Exception::IllegalDataAddress);
         }
     }
 
@@ -126,7 +115,7 @@ fn register_write(
     registers: &mut HashMap<u16, u16>,
     addr: u16,
     values: &[u16],
-) -> Result<(), std::io::Error> {
+) -> Result<(), Exception> {
     for (i, value) in values.iter().enumerate() {
         let reg_addr = addr + i as u16;
         if let Some(r) = registers.get_mut(&reg_addr) {
@@ -134,10 +123,7 @@ fn register_write(
         } else {
             // TODO: Return a Modbus Exception response `IllegalDataAddress` https://github.com/slowtec/tokio-modbus/issues/165
             println!("SERVER: Exception::IllegalDataAddress");
-            return Err(std::io::Error::new(
-                std::io::ErrorKind::AddrNotAvailable,
-                format!("no register at address {reg_addr}"),
-            ));
+            return Err(Exception::IllegalDataAddress);
         }
     }
 
@@ -148,12 +134,14 @@ fn register_write(
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let socket_addr = "127.0.0.1:5502".parse().unwrap();
 
-    tokio::select! {
-        _ = server_context(socket_addr) => unreachable!(),
-        // _ = client_context(socket_addr) => println!("Exiting"),
-    }
+    // tokio::select! {
+    //     _ = server_context(socket_addr) => unreachable!(),
+    //     _ = client_context(socket_addr) => println!("Exiting"),
+    // }
 
-    // Ok(())
+    server_context(socket_addr).await?;
+
+    Ok(())
 }
 
 async fn server_context(socket_addr: SocketAddr) -> anyhow::Result<()> {
@@ -171,44 +159,43 @@ async fn server_context(socket_addr: SocketAddr) -> anyhow::Result<()> {
     Ok(())
 }
 
-#[allow(dead_code)]
-async fn client_context(socket_addr: SocketAddr) {
-    tokio::join!(
-        async {
-            // Give the server some time for starting up
-            tokio::time::sleep(Duration::from_secs(1)).await;
-
-            println!("CLIENT: Connecting client...");
-            let mut ctx = tcp::connect(socket_addr).await.unwrap();
-
-            println!("CLIENT: Reading 2 input registers...");
-            let response = ctx.read_input_registers(0x00, 2).await.unwrap();
-            println!("CLIENT: The result is '{response:?}'");
-            assert_eq!(response, [1234, 5678]);
-
-            println!("CLIENT: Writing 2 holding registers...");
-            ctx.write_multiple_registers(0x01, &[7777, 8888])
-                .await
-                .unwrap();
-
-            // Read back a block including the two registers we wrote.
-            println!("CLIENT: Reading 4 holding registers...");
-            let response = ctx.read_holding_registers(0x00, 4).await.unwrap();
-            println!("CLIENT: The result is '{response:?}'");
-            assert_eq!(response, [10, 7777, 8888, 40]);
-
-            // Now we try to read with an invalid register address.
-            // This should return a Modbus exception response with the code
-            // IllegalDataAddress.
-            println!("CLIENT: Reading nonexisting holding register address... (should return IllegalDataAddress)");
-            let response = ctx.read_holding_registers(0x100, 1).await;
-            println!("CLIENT: The result is '{response:?}'");
-            assert!(response.is_err());
-            // TODO: How can Modbus client identify Modbus exception responses? E.g. here we expect IllegalDataAddress
-            // Question here: https://github.com/slowtec/tokio-modbus/issues/169
-
-            println!("CLIENT: Done.")
-        },
-        tokio::time::sleep(Duration::from_secs(5))
-    );
-}
+// async fn client_context(socket_addr: SocketAddr) {
+//     tokio::join!(
+//         async {
+//             // Give the server some time for starting up
+//             tokio::time::sleep(Duration::from_secs(1)).await;
+//
+//             println!("CLIENT: Connecting client...");
+//             let mut ctx = tcp::connect(socket_addr).await.unwrap();
+//
+//             println!("CLIENT: Reading 2 input registers...");
+//             let response = ctx.read_input_registers(0x00, 2).await.unwrap();
+//             println!("CLIENT: The result is '{response:?}'");
+//             assert_eq!(response, [1234, 5678]);
+//
+//             println!("CLIENT: Writing 2 holding registers...");
+//             ctx.write_multiple_registers(0x01, &[7777, 8888])
+//                 .await
+//                 .unwrap();
+//
+//             // Read back a block including the two registers we wrote.
+//             println!("CLIENT: Reading 4 holding registers...");
+//             let response = ctx.read_holding_registers(0x00, 4).await.unwrap();
+//             println!("CLIENT: The result is '{response:?}'");
+//             assert_eq!(response, [10, 7777, 8888, 40]);
+//
+//             // Now we try to read with an invalid register address.
+//             // This should return a Modbus exception response with the code
+//             // IllegalDataAddress.
+//             println!("CLIENT: Reading nonexisting holding register address... (should return IllegalDataAddress)");
+//             let response = ctx.read_holding_registers(0x100, 1).await;
+//             println!("CLIENT: The result is '{response:?}'");
+//             assert!(response.is_err());
+//             // TODO: How can Modbus client identify Modbus exception responses? E.g. here we expect IllegalDataAddress
+//             // Question here: https://github.com/slowtec/tokio-modbus/issues/169
+//
+//             println!("CLIENT: Done.")
+//         },
+//         tokio::time::sleep(Duration::from_secs(5))
+//     );
+// }
